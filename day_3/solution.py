ddef3968fef784946fe9532d945999bde0b00a15
fredericstrand <strandfrederic@outlook.com>
2024-12-04 09:00:33 +0100

    This one was not good :(

--- day_3/solution.py
+++ day_3/solution.py
@@ -1,14 +1,24 @@
 import re
 
-pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+do_pattern = r"do\(\)"
+dont_pattern = r"don't\(\)"
 
 total_result = 0
-with open("input.txt","r") as data:
-    matches = []
+mul_enabled = True
+
+with open("input.txt", "r") as data:
     for line in data:
-        matches = re.findall(pattern, line)
+        muls = re.split(r"(do\(\)|don't\(\))", line)
+        
+        for mul in muls:
+            mul = mul.strip()
+            if re.fullmatch(do_pattern, mul):
+                mul_enabled = True
+            elif re.fullmatch(dont_pattern, mul):
+                mul_enabled = False
+            elif mul_enabled:
+                matches = re.findall(mul_pattern, mul)
+                total_result += sum(int(i) * int(j) for i, j in matches)
 
-        total_result += sum(int(i) * int(j) for i,j in matches)
-
-
-print(total_result)        
+print(total_result)
