c166426f02486edb254b2b7a63b7f7588e721e33
Frederic Strand <strandfrederic@outlook.com>
2024-12-04 14:03:26 +0100

    final

--- day_4/solution.py
+++ day_4/solution.py
@@ -14,24 +14,24 @@ def part1(xmas_input):
         num_xmas += len(re.findall(pattern[::-1], line))
 
     # Check vertical
-    for col in range(len(lines[0])):
+    cols = max(len(line) for line in lines)
+    for col in range(cols):
         vertical_line = "".join(row[col] for row in lines if col < len(row))
         num_xmas += len(re.findall(pattern, vertical_line))
         num_xmas += len(re.findall(pattern[::-1], vertical_line))
 
     # Check diagonals
     rows = len(lines)
-    cols = len(lines[0])
-    
+
     # Top-left to bottom-right and bottom-right to top-left
     for d in range(-(rows - 1), cols):
-        diagonal = "".join(lines[i][i - d] for i in range(rows) if 0 <= i - d < cols)
+        diagonal = "".join(lines[i][i - d] for i in range(rows) if 0 <= i - d < len(lines[i]))
         num_xmas += len(re.findall(pattern, diagonal))
         num_xmas += len(re.findall(pattern[::-1], diagonal))
 
     # Top-right to bottom-left and bottom-left to top-right
     for d in range(rows + cols - 1):
-        diagonal = "".join(lines[i][d - i] for i in range(rows) if 0 <= d - i < cols)
+        diagonal = "".join(lines[i][d - i] for i in range(rows) if 0 <= d - i < len(lines[i]))
         num_xmas += len(re.findall(pattern, diagonal))
         num_xmas += len(re.findall(pattern[::-1], diagonal))
 
