e104af21217e927f1d04805c2ea92983095c203b
fredericstrand <strandfrederic@outlook.com>
2024-12-04 11:11:36 +0100

    Feel like I am close

---
+++ day_4/solution.py
@@ -0,0 +1,40 @@
+import re
+
+with open("input.txt", "r") as data:
+    xmas_input = data.read()
+
+def part1(xmas_input):
+    lines = xmas_input.split("\n")
+    num_xmas = 0
+    pattern = "XMAS"
+
+    # Check horizontal
+    for line in lines:
+        num_xmas += len(re.findall(pattern, line))
+        num_xmas += len(re.findall(pattern[::-1], line))
+
+    # Check vertical
+    for col in range(len(lines[0])):
+        vertical_line = "".join(row[col] for row in lines if col < len(row))
+        num_xmas += len(re.findall(pattern, vertical_line))
+        num_xmas += len(re.findall(pattern[::-1], vertical_line))
+
+    # Check diagonals
+    rows = len(lines)
+    cols = len(lines[0])
+    
+    # Top-left to bottom-right and bottom-right to top-left
+    for d in range(-(rows - 1), cols):
+        diagonal = "".join(lines[i][i - d] for i in range(rows) if 0 <= i - d < cols)
+        num_xmas += len(re.findall(pattern, diagonal))
+        num_xmas += len(re.findall(pattern[::-1], diagonal))
+
+    # Top-right to bottom-left and bottom-left to top-right
+    for d in range(rows + cols - 1):
+        diagonal = "".join(lines[i][d - i] for i in range(rows) if 0 <= d - i < cols)
+        num_xmas += len(re.findall(pattern, diagonal))
+        num_xmas += len(re.findall(pattern[::-1], diagonal))
+
+    return num_xmas
+
+print(part1(xmas_input))
